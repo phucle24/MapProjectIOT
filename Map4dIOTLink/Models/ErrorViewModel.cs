471b1ba7d305954c206287cc549e8fd7e12d865f
levanphuc <levanphuc24@gmail.com>
2020-10-01 08:55:44 +0700

    Add project files.

---
+++ Map4dIOTLink/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Map4dIOTLink.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
